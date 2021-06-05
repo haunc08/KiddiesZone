36aa563620a1bf18e72bcd0cf05ec3881a999063
LaNeitD18 <phanhuytien14@gmail.com>
2021-06-05 13:40:50 +0700

    Merge branch 'Hau' of https://github.com/LaNeitD18/KiddiesZone into Tien

--- android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
+++ android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
@@ -41,6 +41,11 @@ public class MainActivity extends ReactActivity {
     }
 
     private void hideNavigationBar() {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().getDecorView().setSystemUiVisibility(      View.SYSTEM_UI_FLAG_IMMERSIVE
+      | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+      | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
